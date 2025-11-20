c69b3f0ed0d3c596bc2cc753712889e68d21854d
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-11-21 01:45:28 +0600

    added routes, 404 routes, and 2 api

--- index.js
+++ index.js
@@ -1,11 +1,84 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
+// middleware
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+    console.log(`
+        ${req.method} - ${req.path} from ${req.host} at ${new Date().toLocaleString()}
+    `);
+    next();
+});
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.oguoxou.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// listener
+client
+  .connect()
+  .then(() => {
+    app.listen(port, () => {
+        console.log('Appovate server is running on port', port);
+        console.log('Appovate server is connected with DB');
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// database and collection
+const db = client.db('appovate_db');
+const appsCol = db.collection('apps');
+
+// app routes
 app.get('/', (req, res) => {
-    res.send('Hello from appovate server');
+    res.json({status: 'ok', message: 'Welcome to Appovate Server'});
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+// defined route / API
+app.get('/apps', async(req, res) => {
+    try{
+        const cursor = appsCol.find({});
+        const allApps = await cursor.toArray();
+        res.send(allApps);
+    } catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+
+// get app by id
+app.get('/apps/:id', async(req, res) => {
+    try{
+        const appId = req.params.id;
+        if(appId.length !== 24){
+            res.status(400).json({ error: "Invalid ID" });
+            return;
+        }
+        const query = { _id: new ObjectId(appId) };
+        const app = await appsCol.findOne(query);
+        res.json(app);
+    } catch(error){
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+// 404, no route
+app.all(/.*/, (req, res) =>{
+    res.status(404).json({status: 404, error: 'API Not Found!'});
 });
